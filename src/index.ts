90b090ccd4552a98d1712669d75c38e29d7a1a8a
Ronald Rugube <rrugube69@gmail.com>
2021-11-04 13:02:24 +0200

    First Commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,44 @@
 import { GraphQLServer } from 'graphql-yoga'
 import { importSchema } from 'graphql-import'
-import { Prisma } from './generated/prisma'
+import { Prisma, User } from './generated/prisma';
 import { Context } from './utils'
+import { Keypair } from 'stellar-sdk'
+import { AES } from 'crypto-js'
 
 const resolvers = {
   Query: {
+    user(_, { username }, context: Context, info) {
+      return context.db.query.users(
+        {
+          where: {
+            username
+          }
+        },
+        info
+      )
+    }
   },
   Mutation: {
+    signup(_, { username }, context: Context, info) {
+      const keypair = Keypair.random()
+
+      const configCryptoScret = 'StellasIsaAwesome'
+      const secret = AES.encrypt(
+        keypair.secret(),
+        configCryptoScret
+      
+      ).toString()
+      const data = {
+        username,
+        stellarAccount: keypair.publicKey(),
+        stellarSeed: keypair.secret()
+      }
+
+      return context.db.mutation.createUser(
+        { data },
+        info
+      )
+    },
   },
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,44 @@
 import { Prisma } from './generated/prisma'
+import {
+  Keypair, // Keypair represents public and secret keys.
+  Networks, // Network provides helper methods to get the passphrase or id for different stellar networks.
+  Operation, // Operation helps you represent/build operations in Stellar network.
+  Server, // Server handles the network connections.
+  TransactionBuilder // Helps you construct transactions.
+} from 'stellar-sdk'
 
 export interface Context {
   db: Prisma
   request: any
+}
+
+try {
+  // Tell the Stellar SDK you are using the testnet
+  // point to testnet host
+  const stellarServer = new Server('https://horizon-testnet.stellar.org');
+
+  // Never put values like the an account seed in code.
+const provisionerKeyPair = Keypair.fromSecret('SAZZGPURDGJO7D2G66CGJB6BDAYUDUIZAWKHU75NWGHH6BX25UEIESC2')
+
+  // Load account from Stellar
+  const provisioner = await stellarServer.loadAccount(provisionerKeyPair.publicKey())
+
+  console.log('creating account in ledger', Keypair.publicKey())
+  const transaction = new TransactionBuilder(provisioner)
+        .addOperation(
+          // Operation to create new accounts
+          Operation.createAccount({
+            destination: Keypair.publicKey(),
+            startingBalance: '2'
+          })
+        ).build()
+
+  // Sign the transaction above
+  transaction.sign(provisionerKeyPair)
+
+  // Submit transaction to the server
+  const result = await stellarServer.submitTransaction(transaction);
+  console.log('Account created: ', result)
+} catch (e) {
+  console.log('Stellar account not created.', e)
 }
